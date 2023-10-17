b9664dd30f926d980c3921c15a95bf670ad24621
Julian Koehn <me@julian.pro>
2023-10-17 18:39:49 +0200

    feat: footerino

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [react(), tailwind()],
   site: 'https://juliankoehn.github.io',
-  base: '/',
+  base: '/julian.pro',
 });
