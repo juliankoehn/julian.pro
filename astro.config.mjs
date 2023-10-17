ec32a2426cdc908afdf3dc46f1a247749705876c
Julian Koehn <me@julian.pro>
2023-10-17 19:23:42 +0200

    feat: cname

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-  site: 'https://juliankoehn.github.io',
+  site: 'https://julian.pro',
 });
