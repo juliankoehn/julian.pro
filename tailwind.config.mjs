2d9b77cbc205380ced967169f54d1dcf1043fe20
Julian Koehn <me@julian.pro>
2023-10-17 22:27:52 +0200

    COLORS

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,10 +7,51 @@ export default {
 	theme: {
 		extend: {
             colors: {
-                background: '#f7fff7',
-                foreground: "#313638",
-                secondary: colors.orange[500], // '#ffe66d',
-            }
+                background: "#162847", // '#f7fff7',
+                foreground: colors.orange[100], // "#FFE5A6", // "#313638",
+                secondary: {
+                    background: colors.orange[500],
+                    foreground: colors.orange[100],
+                }, // '#ffe66d',
+            },
+            typography: (theme) => ({
+                primary: {
+                    css: {
+                        '--tw-prose-body': theme('colors.orange[200]'),
+                        '--tw-prose-headings': theme('colors.orange[900]'),
+                        '--tw-prose-lead': theme('colors.orange[700]'),
+                        '--tw-prose-links': theme('colors.orange[900]'),
+                        '--tw-prose-bold': theme('colors.orange[900]'),
+                        '--tw-prose-counters': theme('colors.orange[600]'),
+                        '--tw-prose-bullets': theme('colors.orange[400]'),
+                        '--tw-prose-hr': theme('colors.orange[300]'),
+                        '--tw-prose-quotes': theme('colors.orange[900]'),
+                        '--tw-prose-quote-borders': theme('colors.orange[300]'),
+                        '--tw-prose-captions': theme('colors.orange[700]'),
+                        '--tw-prose-code': theme('colors.orange[900]'),
+                        '--tw-prose-pre-code': theme('colors.orange[100]'),
+                        '--tw-prose-pre-bg': theme('colors.orange[900]'),
+                        '--tw-prose-th-borders': theme('colors.orange[300]'),
+                        '--tw-prose-td-borders': theme('colors.orange[200]'),
+                        '--tw-prose-invert-body': theme('colors.orange[200]'),
+                        '--tw-prose-invert-headings': theme('colors.white'),
+                        '--tw-prose-invert-lead': theme('colors.orange[300]'),
+                        '--tw-prose-invert-links': theme('colors.white'),
+                        '--tw-prose-invert-bold': theme('colors.white'),
+                        '--tw-prose-invert-counters': theme('colors.orange[400]'),
+                        '--tw-prose-invert-bullets': theme('colors.orange[600]'),
+                        '--tw-prose-invert-hr': theme('colors.orange[700]'),
+                        '--tw-prose-invert-quotes': theme('colors.orange[100]'),
+                        '--tw-prose-invert-quote-borders': theme('colors.orange[700]'),
+                        '--tw-prose-invert-captions': theme('colors.orange[400]'),
+                        '--tw-prose-invert-code': theme('colors.white'),
+                        '--tw-prose-invert-pre-code': theme('colors.orange[300]'),
+                        '--tw-prose-invert-pre-bg': 'rgb(0 0 0 / 50%)',
+                        '--tw-prose-invert-th-borders': theme('colors.orange[600]'),
+                        '--tw-prose-invert-td-borders': theme('colors.orange[700]'),
+                    },
+                }
+            }),
         },
 	},
 	plugins: [
@@ -20,7 +61,7 @@ export default {
                 '.sticky-border': {
                     borderColor: theme('colors.foreground'),
                     borderRadius: theme('borderRadius.xl'),
-                    boxShadow: `4px 4px ${theme('colors.foreground')}`,
+                    boxShadow: `4px 4px ${theme('colors.white')}`,
                     position: 'relative',
                 },
                 '.sticky-border .page-scroll-progress': {
@@ -31,7 +72,7 @@ export default {
                     position: 'absolute'
                 },
                 '.shadow-fill': {
-                    boxShadow: `4px 4px 0 ${theme('colors.foreground')}`
+                    boxShadow: `4px 4px 0 ${theme('colors.white')}`
                 },
                 '.shadow-fill:hover': {
                     '--tw-translate-x': '2px',
