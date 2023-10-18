c539eeaedb563493adfc6fc5006979860ccbe031
Julian Koehn <me@julian.pro>
2023-10-18 23:06:44 +0200

    feat: prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 import { defineConfig } from 'astro/config';
 import { remarkReadingTime } from './plugins/remark-reading-time.mjs';
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), prefetch()],
   site: 'https://julian.pro',
   markdown: {
-    remarkPlugins: [remarkReadingTime],
-  },
+    remarkPlugins: [remarkReadingTime]
+  }
 });
