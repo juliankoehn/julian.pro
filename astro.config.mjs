3f6594aade6f3660190bffd58eae43e761ec21f5
Julian Koehn <me@julian.pro>
2023-10-19 13:20:37 +0200

    feat: sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,15 @@ import { defineConfig } from 'astro/config';
 import { remarkReadingTime } from './plugins/remark-reading-time.mjs';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
 import prefetch from "@astrojs/prefetch";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), prefetch()],
+  integrations: [react(), tailwind(), prefetch(), sitemap()],
   site: "https://julian.pro",
   markdown: {
-    remarkPlugins: [
-      remarkReadingTime,
-    ],
-  },
+    remarkPlugins: [remarkReadingTime]
+  }
 });
